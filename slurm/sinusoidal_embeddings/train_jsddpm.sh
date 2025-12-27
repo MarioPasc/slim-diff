60fcbf405fd4e303d7421e3fb883fc1cbabcef06
MPascual <mario.pg02@gmail.com>
2025-12-27 11:13:07 +0100

    Sinusoidal embeddings

--- slurm/sinusoidal_embeddings/train_jsddpm.sh
+++ slurm/sinusoidal_embeddings/train_jsddpm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH -J log_cfg_train_jsddpm_flair_soco
+#SBATCH -J log_jsddpm_sinusoidal_embeddings_flair_soco
 #SBATCH --time=1-12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -27,13 +27,13 @@ echo "Job started at: $(date)"
 # ========================================================================
 # DISTRIBUTED TRAINING CONFIGURATION
 # ========================================================================
-EXPERIMENT_NAME="jsddpm_cfg"
+EXPERIMENT_NAME="jsddpm_sinusoidal_embeddings"
 CONDA_ENV_NAME="jsddpm"
 
 REPO_SRC="/mnt/home/users/tic_163_uma/mpascual/fscratch/repos/js-ddpm-epilepsy"
 DATA_SRC="/mnt/home/users/tic_163_uma/mpascual/fscratch/datasets/epilepsy"
 RESULTS_DST="/mnt/home/users/tic_163_uma/mpascual/fscratch/results/${EXPERIMENT_NAME}"
-CONFIG_FILE="${REPO_SRC}/slurm/cfg/${EXPERIMENT_NAME}.yaml"
+CONFIG_FILE="${REPO_SRC}/slurm/sinusoidal_embeddings/${EXPERIMENT_NAME}.yaml"
 
 # Dynamic GPU assignment
 export CUDA_VISIBLE_DEVICES=0
